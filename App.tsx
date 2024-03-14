493ab1f5c5a82c0e645506761d57d54f9b3f2d56
Esteban <jestebangarciac23@gmail.com>
2024-03-13 21:59:24 -0500

    correction

--- App.tsx
+++ App.tsx
@@ -2,20 +2,20 @@ import React from "react";
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import DrivingTest from "./src/components/DrivingTest";
-import VehicleList from "./src/components/VehicleList";
+//import VehicleList from "./src/components/VehicleList";
 import VehicleSearch from "./src/components/VehicleSearch";
 //import de esteban
 //import DrivingTest from "./src/components/DrivingTest";
 import VehicleList from "./src/components/VehicleList";
 //import VehicleSearch from "./src/components/VehicleSearch";
 //import de luisa 
-//import Price from './src/components/Price';
-//import StartUp from "./src/components/StartUp.jsx";
-//import Contact from "./src/components/Contact";
+import Price from './src/components/Price';
+import StartUp from "./src/components/StartUp.jsx";
+import Contact from "./src/components/Contact";
 //import de andres
-//import HistoryBackground from './src/components/serviceHistoryModule/HistoryBackground'
-//import NotificationBackground from './src/components/notificationModule/NotificationBackground'
-//import ServiceBackground from './src/components/serviceRequestModule/ServiceBackground'        
+import HistoryBackground from './src/components/serviceHistoryModule/HistoryBackground'
+import NotificationBackground from './src/components/notificationModule/NotificationBackground'
+import ServiceBackground from './src/components/serviceRequestModule/ServiceBackground'        
 
 export const App =() => {
   return(
@@ -23,15 +23,11 @@ export const App =() => {
     //<Logo/>
     //<DrivingTest/>
     //<VehicleSearch/>
-<<<<<<< Updated upstream
     <SafeAreaProvider>
-    <VehicleList/>
+    <ServiceBackground/>
     </SafeAreaProvider>
-
    // <VehicleList/>
-=======
-    <VehicleList/>
->>>>>>> Stashed changes
+    //<VehicleList/>
    //luisa
    //<StartUp />
    //<Price/>
